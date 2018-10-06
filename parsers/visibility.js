4a47510dbfb03d0f6d6979c9312738efa8f14eb0
raideer <idmania1@gmail.com>
2018-10-06 15:56:30 +0300

    Fixed visibility parser

--- parsers/visibility.js
+++ parsers/visibility.js
@@ -3,7 +3,7 @@ const { UNITS } = require("../texts");
 
 module.exports = class VisibilityParser extends Parser {
     static parse(metar) {
-        const match = ` ${metar} `.match(/\s([0-9]{4})|((?:[0-9]\/)?[0-9]{1,2})(SM|KM)\s/);
+        const match = ` ${metar} `.match(/\s([0-9]{4})\s|\s((?:[0-9]{1,2}\/)?[0-9]{1,2})(SM|KM)\s/);
 
         if (!match) {
             return {
@@ -11,24 +11,37 @@ module.exports = class VisibilityParser extends Parser {
             };
         }
 
-        const units = UNITS[match[3]] || "meters";
-        const distance = parseInt(match[1] || match[2], 10);
+        const unit = UNITS[match[3]] || "meter";
+        let distance;
+
+        if (match[2] && match[2].includes("/")) {
+            const parts = match[2].split("/").map(part => parseInt(part, 10));
+
+            distance = parts[0] / parts[1];
+        } else {
+            distance = parseInt(match[1] || match[2], 10);
+        }
 
         let distanceMeters;
         let distanceFeet;
 
-        if (units === "meters") {
+        if (unit === "meter") {
             distanceMeters = distance;
             distanceFeet = Math.round(distance * 3.2808);
-        } else {
-            distanceFeet = distance;
-            distanceMeters = Math.round(distance / 3.2808);
+        } else if (unit === "kilometer") {
+            distanceMeters = distance * 1000;
+            distanceFeet = Math.round(distanceMeters * 3.2808);
+        } else if (unit === "statute mile") {
+            distanceFeet = distance * 5280;
+            distanceMeters = Math.round(distanceFeet * 0.3048);
         }
 
         return {
             visibility: {
                 meters: distanceMeters,
-                feet: distanceFeet
+                feet: distanceFeet,
+                miles: Math.round(distanceFeet / 5280 * 100) / 100,
+                kilometers: Math.round(distanceMeters / 10) / 100
             }
         };
     }
