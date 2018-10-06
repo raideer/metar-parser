9f4e348964f75369c45db0d80a3ee5fb86e04ec9
raideer <idmania1@gmail.com>
2018-10-06 15:55:05 +0300

    Added NOSIG parser

---
+++ parsers/nosig.js
@@ -0,0 +1,11 @@
+const Parser = require("../parser");
+
+module.exports = class NosigParser extends Parser {
+    static parse(metar) {
+        const match = ` ${metar} `.match(/\sNOSIG\s/);
+
+        return {
+            nosig: match ? match[1] : false
+        };
+    }
+};
